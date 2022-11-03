7e8b3b241389a76a02f3cb536e87f3c4d16dfa56
INDHIRAJITH2000 <114467856+INDHIRAJITH2000@users.noreply.github.com>
2022-11-03 13:57:48 +0530

    Add files via upload

---
+++ volkswagen1/script.js
@@ -0,0 +1,12 @@
+function myfunction(){
+    var word=document.getElementById("word").value;
+    var passw=document.getElementById("passw").value;
+    if(word=="Indraa"&& passw=="Indraa")
+    {
+        alert('login success');
+    }
+    else{
+        alert('login failed');
+    }
+
+}
